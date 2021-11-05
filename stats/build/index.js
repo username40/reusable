3645702a6305271139c92581a5082331ad4e8e43
username40 <mr.v.e.romantsov@gmail.com>
2021-11-05 18:06:31 +0300

    create environment

---
+++ stats/build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('hello TS!');
+console.log('hello TS!');
+console.log('hello TS!');
---
+++ stats/src/index.ts
@@ -0,0 +1,3 @@
+console.log('hello TS!');
+console.log('hello TS!');
+console.log('hello TS!');
