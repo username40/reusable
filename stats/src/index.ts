b00ab54ee0ee6ebc8f8f6eefbbe296a3021c768e
username40 <mr.v.e.romantsov@gmail.com>
2021-11-05 21:20:31 +0300

    create stats

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -8,4 +8,14 @@ const matches = fs.readFileSync('football.csv', {
     .split('\n')
     .map((row: string):string[] => row.split(','));
 
-console.log(matches);
+let manUnitedWins = 0
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    } else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Manchester United win: ${manUnitedWins} times`);
