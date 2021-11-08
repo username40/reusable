c5129e49af5eb9eff3433b47705cc8b9703e9c0f
username40 <mr.v.e.romantsov@gmail.com>
2021-11-08 22:06:26 +0300

    start refactoring

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -8,12 +8,16 @@ const matches = fs.readFileSync('football.csv', {
     .split('\n')
     .map((row: string):string[] => row.split(','));
 
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
 let manUnitedWins = 0
 
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === homeWin) {
         manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === 'A') {
+    } else if (match[2] === 'Man United' && match[5] === awayWin) {
         manUnitedWins++;
     }
 }
