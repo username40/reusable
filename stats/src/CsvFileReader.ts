eeb88503996933e77fbede64987ba9b2e5104240
username40 <mr.v.e.romantsov@gmail.com>
2021-11-10 22:26:52 +0300

    implement util

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import {dateStringToDate} from "./util";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -9,6 +10,15 @@ export class CsvFileReader {
       encoding: 'utf-8'
     })
     .split('\n')
-    .map((row: string): string[] => row.split(','));
+    .map((row: string): string[] => row.split(','))
+    .map((row: string[]): any => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+      ]
+    })
   }
 }
