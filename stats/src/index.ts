1de315edb388f561834b5ea2533e596781523ae1
username40 <mr.v.e.romantsov@gmail.com>
2021-11-10 21:52:44 +0300

    make 1 more class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,11 @@
 // for correct work install type definition files
 // npm i @types/node
-import fs from 'fs';
+// import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs.readFileSync('football.csv', {
-	encoding: 'utf-8'
-})
-.split('\n')
-.map((row: string): string[] => row.split(','));
+
+const reader = new CsvFileReader('football.csv')
+reader.read()
 
 // enum
 enum MatchResult {
@@ -17,7 +16,7 @@ enum MatchResult {
 
 let manUnitedWins = 0
 
-for (let match of matches) {
+for (let match of reader.data) {
 	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
 		manUnitedWins++;
 	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
