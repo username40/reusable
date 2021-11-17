18699a69893ec329d72804835763d6f4f5dcc5ff
username40 <mr.v.e.romantsov@gmail.com>
2021-11-17 23:07:50 +0300

    create backup, split logic of return array

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -17,17 +17,18 @@ var CsvFileReader = /** @class */ (function () {
         })
             .split('\n')
             .map(function (row) { return row.split(','); })
-            .map(function (row) {
-            return [
-                util_1.dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6]
-            ];
-        });
+            .map(this.mapRow);
+    };
+    CsvFileReader.prototype.mapRow = function (row) {
+        return [
+            util_1.dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6]
+        ];
     };
     return CsvFileReader;
 }());
